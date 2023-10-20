bc330b3c5e8d9ca8a557f0618b73f13436f77014
MirkoGLostia <giggi.torre@mail.com>
2023-10-20 14:16:01 +0200

    Java Shop prodotto: attributi e costruttore iniziale

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,4 +1,27 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+
+    // attributi
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
+
+
+    // costruttori
+
+    public Prodotto(String nome, String descrizione, double prezzo, double iva) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+
+    // metodi
+
+
+
 }
